37e710a02aa5199826d0b83e8e850435f03e1896
whytdan <aktan.r.a@gmail.com>
2020-07-31 00:29:24 +0600

    add smptp sender client

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ const navSlide = function(){
   });
 }
 navSlide();
+
+function sendEmail({fullName, email, subject, message }) {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "utlabsender@gmail.com",
+    Password: "utl@b123",
+    To: "Aktan.r.a@gmail.com",
+    From: "utlabsender@gmail.com",
+    Subject: "New request",
+    Body: `Client FullName: ${fullName} <br/> 
+           Email: ${email} <br/>
+           Subject: ${subject} <br/>
+           Message: ${message}`,
+  })
+}
